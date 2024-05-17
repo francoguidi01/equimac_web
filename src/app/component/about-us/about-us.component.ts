e08be28c6ab8fbba159d9f3f7496615955e5a57a
Kevin <kevintolosa2000@gmail.com>
2024-05-17 18:34:18 -0300

    Continuacion el lunes

--- src/app/component/about-us/about-us.component.ts
+++ src/app/component/about-us/about-us.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
+
 export class AboutUsComponent {
+
+
 
 }
