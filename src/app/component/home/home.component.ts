3b6bc7b89aebd1441be3bb8ed8ff43bc2764e2d9
Kevin <kevintolosa2000@gmail.com>
2024-07-21 05:08:39 -0300

    I modified products in the home and products view

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -17,9 +17,7 @@ import { DarkModeService } from '../../services/dark-mode.service';
 export class HomeComponent {
   product: Product | null = new Product();
   listProduct: any;
-  listProduct1: any;
-  listProduct2: any;
-  listProduct3: any;
+  randomProducts: Product[] = [];
   isDarkMode: boolean = false;
 
   constructor(private service: ProductService, private route: ActivatedRoute, private darkModeService: DarkModeService) {
@@ -43,14 +41,8 @@ export class HomeComponent {
   get_all() {
     this.listProduct = this.service.getAll();
     this.listProduct.sort(() => Math.random() - 0.5);
-
-    const third = Math.floor(this.listProduct.length / 3);
-    this.listProduct1 = this.listProduct.slice(0, third);
-    this.listProduct2 = this.listProduct.slice(third, 2 * third);
-    this.listProduct3 = this.listProduct.slice(2 * third, this.listProduct.length);
-
-    console.log(this.listProduct1);
-    console.log(this.listProduct2);
-    console.log(this.listProduct3);
+    this.randomProducts = this.listProduct.slice(0, 5);
+    console.log(this.randomProducts);
   }
+
 }
