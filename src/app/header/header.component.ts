45cb105398c49895ab8b2025b7f437de8463cfb9
Kevin <kevintolosa2000@gmail.com>
2024-07-29 19:47:52 -0300

    Final changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { DarkModeService } from '../services/dark-mode.service';
 
@@ -7,11 +7,16 @@ import { DarkModeService } from '../services/dark-mode.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit{
 
   isDarkMode: boolean = false;
   listProduct: any;
   filteredProducts: any;
+  isSearchActive: boolean = false;
+
+@ViewChild('searchInput') searchInput!: ElementRef<HTMLInputElement>;
+
 
   constructor(private productService: ProductService, private darkModeService: DarkModeService) {}
 
@@ -27,16 +32,24 @@ export class HeaderComponent implements OnInit{
     }
   }
 
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (!this.isSearchActive || target.closest('.search')) return;
+
+    this.isSearchActive = false;
+  }
+
   searchProducts(event: any) {
     const query = event.target.value;
     if (query) {
-      console.log(query)
       this.filteredProducts = this.productService.searchProducts(query);
+      this.isSearchActive = true; 
     } else {
       this.filteredProducts = [];
+      this.isSearchActive = false;
     }
   }
-
 
   toggleDarkMode(): void {
     this.isDarkMode = !this.isDarkMode;
